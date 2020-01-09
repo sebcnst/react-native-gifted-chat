b00146f8e7fe6cc66c7cad596986cd753cb8ad1c
Cnst <sebcnstt@gmail.com>
2020-01-09 19:40:59 +0100

    update style toolbar

--- src/InputToolbar.tsx
+++ src/InputToolbar.tsx
@@ -20,8 +20,9 @@ const styles = StyleSheet.create({
     borderTopWidth: StyleSheet.hairlineWidth,
     borderTopColor: Color.defaultColor,
     backgroundColor: Color.white,
-    bottom: 0,
-    left: 0,
+    bottom: 40,
+    width: '90%',
+    left: '5%',
     right: 0,
   },
   primary: {
